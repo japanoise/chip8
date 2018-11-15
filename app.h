a5455298390506ae05f6bfdb7f449b1060711f05
japanoise <japanoise@users.noreply.github.com>
2018-11-15 22:02:37 +0000

    Personal preferences

--- app.h
+++ app.h
@@ -1,12 +1,12 @@
 #define WINDOW_CAPTION "CHIP-8 Machine"
 
-#define FG_COLOR        0xAAAAFF
-#define BG_COLOR        0x000055
+#define FG_COLOR        0xE85A4F
+#define BG_COLOR        0xEAE7DC
 
 #define NO_OPEN_LOG     1
 #define LOG_FILE_NAME   stderr
 #define EPX_SCALE       0
-#define SCREEN_SCALE    2
+#define SCREEN_SCALE    4
 
 #define SCREEN_WIDTH    128
 #define SCREEN_HEIGHT   64
